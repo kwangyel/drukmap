86eb8323d14574a9a254da0efaf2c2c6519dcec2
unknown <kwangyel@gmail.com>
2020-09-24 07:34:54 +0600

    bb

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -1,4 +1,6 @@
 // export const API_URL = 'http://covidoutpass.local/api';
 // export const API_URL = 'https://outpassdashboard.desuung.org.bt/api';
 export const API_DEV = 'http://localhost:8080/map';
-export const API_URL = 'http://localhost:8080/map';
+// export const API_URL = 'http://localhost:8080/map';
+
+export const API_URL = 'https://zhichar.myddns.rocks/mpi/map';
