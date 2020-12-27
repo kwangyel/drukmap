7f7050feb85088bb12681a9e12fa7efd33ebd185
kwangyel <kwangyel@gmail.com>
2020-12-27 12:33:52 +0600

    dummy

--- src/app/map/directionDialog.component.ts
+++ src/app/map/directionDialog.component.ts
@@ -1,6 +1,7 @@
 import { Component, Inject } from '@angular/core';
 import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
 import { EventEmitter } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder, Form } from '@angular/forms';
 
 @Component({
   selector: 'direction-dialog',
@@ -10,13 +11,34 @@ import { EventEmitter } from '@angular/core';
 export class DirectionDialog {
     poiName:string;
     streetName: string;
+    originform: FormGroup;
+    destinationform: FormGroup;
+    OriginOptions = [];
+    DestinationOptions = [];
 
     constructor(private dialogRef: MatDialogRef<DirectionDialog>,
-        @Inject(MAT_DIALOG_DATA) public data: {poiName: string,street: string}) { }
+        @Inject(MAT_DIALOG_DATA) public data: {poiName: string,street: string},
+        private fb : FormBuilder,
+        ) { }
     ngOnInit() {
         this.poiName = this.data.poiName;
         this.streetName = this.data.street;
+        this.originform = this.fb.group({
+        origin:[]
+        });
+        this.destinationform = this.fb.group({
+        destination:new FormControl()
+        });
     }
+    poiShare(){}
+    onOriginSelected(){}
+    onDestinationSelected(){}
+    getOptionText(){}
+    onOriginSearch(){}
+    onDestinationSearch(){}
+    getDirections(){}
+
+    
 
 
 }
