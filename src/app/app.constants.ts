739d6988063d4470de5e6bd1b31ed7382cfae954
kwangyel <kwangyel@gmail.com>
2021-05-13 14:21:08 +0600

    Update app.constants.ts
    app url update

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -3,6 +3,6 @@
 // export const API_DEV = 'http://localhost:8080/map';
 // export const API_URL = 'http://localhost:8080/map';
 
-export const API_URL = 'https://zhichar.myddns.rocks/mpi/map';
-export const API_DEV= 'https://zhichar.myddns.rocks/mpi/map';
+export const API_URL = 'https://zhichar-pling.ddnsfree.com/mpi/map';
+export const API_DEV= 'https://zhichar-pling.ddnsfree.com/mpi/map';
 export const API_LOCAL = "http://localhost:8090/map";
